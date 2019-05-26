df90b75043784c85876a08fed7698b547233a2ad
mhwaymhway <mhwayei917@gmail.com>
2019-05-26 11:08:04 +0630

    modif post name

--- routes/post.js
+++ routes/post.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-var Post = require('../model/post');
+var Post = require('../model/Post');
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
